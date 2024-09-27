9c9ba718c11fc7433adbf2a198555feb41e4ad64
ritamrao <ritam22001@gmail.com>
2024-09-27 16:09:02 +1000

    git init

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://bookcatalog_8jg7_user:kgw1X95XC0P96fdmzwAOWU112YNP9w6l@dpg-crjageu8ii6s73fe2na0-a.oregon-postgres.render.com/bookcatalog_8jg7')
